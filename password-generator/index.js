1a52f2c9adb36fdbb4f36e05d200302febb17888
Silvio Schwarz <admin@silvioschwarz.com>
2023-05-02 13:24:53 +0200

    type="module"

--- password-generator/index.js
+++ password-generator/index.js
@@ -1,65 +1,74 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
 
-const appSettings = {
-    databaseURL: "https://realtime-database-81fcb-default-rtdb.europe-west1.firebasedatabase.app/"
+
+let generatePasswordBtn = document.getElementById("generatePassword-btn");
+let password1 = document.getElementById("pass1");
+let password2 = document.getElementById("pass2");
+
+let passLength = document.getElementById("passlen")
+passLength.textContent = document.getElementById("volume").value
+
+ document.getElementById("volume").oninput = function() {
+ passLength.textContent = this.value;
 }
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const shoppingListInDB = ref(database, "shoppingList")
 
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
-const shoppingListEl = document.getElementById("shopping-list")
-
-addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
+function generatePassword (){
+    let len = document.getElementById("volume").value
+    let password = ""
+    let charactersToUse =[]
     
-    push(shoppingListInDB, inputValue)
+    let uppercase = document.getElementById("uppercase").checked
+    let lowercase = document.getElementById("lowercase").checked
+    let numeric = document.getElementById("numeric").checked
+    let special = document.getElementById("special").checked
     
-    clearInputFieldEl()
-})
-
-onValue(shoppingListInDB, function(snapshot) {
-    if (snapshot.exists()) {
-        let itemsArray = Object.entries(snapshot.val())
-    
-        clearShoppingListEl()
-        
-        for (let i = 0; i < itemsArray.length; i++) {
-            let currentItem = itemsArray[i]
-            let currentItemID = currentItem[0]
-            let currentItemValue = currentItem[1]
-            
-            appendItemToShoppingListEl(currentItem)
-        }    
+    if (uppercase || lowercase || numeric || special){
+        if(uppercase){
+            charactersToUse.push(...characters.slice(0,26))
+        }
+        if(lowercase){
+            charactersToUse.push(...characters.slice(26,52))
+        }
+        if(numeric){
+            charactersToUse.push(...characters.slice(52,62))
+        }
+        if(special){
+            charactersToUse.push(...characters.slice(62,characters.length))
+            }
     } else {
-        shoppingListEl.innerHTML = "No items here... yet"
+       charactersToUse.push(...characters.slice(0,26))
+       document.getElementById("uppercase").checked = true; 
+   }
+    
+    
+    
+    for (let i=0; i<len; i++){
+        password += charactersToUse[Math.floor(Math.random()*charactersToUse.length)]
     }
+     return password
+}
+
+generatePasswordBtn.addEventListener("click", function(){
+    password1.textContent = generatePassword(5);
+    password2.textContent = generatePassword(5)
 })
 
-function clearShoppingListEl() {
-    shoppingListEl.innerHTML = ""
-}
 
-function clearInputFieldEl() {
-    inputFieldEl.value = ""
-}
 
-function appendItemToShoppingListEl(item) {
-    let itemID = item[0]
-    let itemValue = item[1]
-    
-    let newEl = document.createElement("li")
-    
-    newEl.textContent = itemValue
-    
-    newEl.addEventListener("click", function() {
-        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
-        
-        remove(exactLocationOfItemInDB)
-    })
-    
-    shoppingListEl.append(newEl)
-}
+
+function copyDivToClipboard(passid) {
+    passEl = document.getElementById(passid).innerText;
+    if (passEl !=""){
+        var r = document.createRange();
+        r.selectNode(document.getElementById(passid));
+        window.getSelection().removeAllRanges();
+        window.getSelection().addRange(r);
+        document.execCommand('copy');
+        window.getSelection().removeAllRanges();
+
+        // Alert the copied text
+        alert("Copied the text: " + r);
+        }     
+    }
